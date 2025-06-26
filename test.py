8e663b8a29cc2c6fa9ba03467d818f7b14e7e262
Graham Dirks <grahamdirks11@gmail.com>
2025-06-26 15:44:31 -0700

    Made slight changes to the code Downloaded software to facillitate communication between computer and Oscilliscope.

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import keyoscacquire as koa
+import time
 
-scope = koa.oscilloscope.Oscilloscope(address="")
-
-scope.set_options_get_trace_save("TestTrace.png", num_points=1000)
-scope.plot_trace()
+scope = koa.oscilloscope.Oscilloscope(address='USB0::10893::6006::MY58262555::INSTR', )
+scope.savepng = False
+for i in range(10):
+    scope.set_options_get_trace(num_points=1000)
+    scope.save_trace(f"./OscopeOut/TestTrace3_{i}", ext=".csv")
+    time.sleep(0.0001)
